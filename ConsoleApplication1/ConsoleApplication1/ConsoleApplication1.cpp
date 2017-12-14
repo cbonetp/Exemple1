f24e20e1ebc5beb18a8bb623bf90bdf04f05c96e
Carles.bonet <Carles.bonet@escolapia.cat>
2017-12-15 00:11:51 +0100

    Commit2

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int x;
+	int y;
+	x = 5;
+	y = 10;
+	cout << x + y;
+	system("pause");
+    return 0;
+}
+
